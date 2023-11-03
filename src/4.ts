6d79ca3d9365a1be53dba01b689d07280a45bca3
gladja <gladkih.alexander@gmail.com>
2023-11-03 19:20:55 +0200

    task03

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,25 @@
+
+class Key {
+    constructor( private signature: number) {
+    this.signature =  Math.random();
+    }
+
+    getSignature() {
+        return this.signature
+    }
+}
+
+class Person extends Key{
+    constructor(signature: number, key: number) {
+        super(signature)
+        // this.key = signature
+    }
+
+    getKey(key) {
+        return key
+    }
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
